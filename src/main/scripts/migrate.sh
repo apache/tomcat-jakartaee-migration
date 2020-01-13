31d728d82cf2b1f4afc571d6e7e814576988ee00
Mark Thomas <markt@apache.org>
2020-01-13 15:05:29 +0000

    Fix various issues when doing a clean build

--- src/main/scripts/migrate.sh
+++ src/main/scripts/migrate.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Assumes current layout of Maven's target directory
-cd ..
-
 # Assumes java is on the path
-java -cp lib/* org.apache.tomcat.jakartaee.Migration "$@"
+java -cp "../lib/*" org.apache.tomcat.jakartaee.Migration "$@"
