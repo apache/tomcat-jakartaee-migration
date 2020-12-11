e8e3318bac8851ee757af58011f21b3b7d28c97d
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-12-11 13:36:03 +0200

    Re-work how the bin folder path is calculated
    Suggested-by: Rainer Jung

--- src/main/scripts/migrate.sh
+++ src/main/scripts/migrate.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-BIN_FOLDER=`dirname $PWD/$0`
+mydir="$PWD"
+cd `dirname "$0"`
+BIN_FOLDER="$PWD"
+cd "$mydir"
 
 # Assumes java is on the path
 java -cp "$BIN_FOLDER/../lib/*" org.apache.tomcat.jakartaee.MigrationCLI "$@"
