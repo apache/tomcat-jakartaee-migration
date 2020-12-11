000c876ea3a1e700df2fffef70b29d9c3a9dfef2
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-12-11 09:22:22 +0200

    Make migrate.sh usable from any directory
    Until now one has to `cd` to the bin/ folder to be able to execute migrate.sh, otherwise lib/ folder won't be found

--- src/main/scripts/migrate.sh
+++ src/main/scripts/migrate.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+BIN_FOLDER=`dirname $PWD/$0`
+
 # Assumes java is on the path
-java -cp "../lib/*" org.apache.tomcat.jakartaee.MigrationCLI "$@"
+java -cp "$BIN_FOLDER/../lib/*" org.apache.tomcat.jakartaee.MigrationCLI "$@"
