fed139dd3f7351b50a7196107cbdb48585232747
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-12-14 15:17:35 +0200

    Simplify migrate.sh
    Suggested-by: Christopher Schultz

--- src/main/scripts/migrate.sh
+++ src/main/scripts/migrate.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-mydir="$PWD"
-cd `dirname "$0"`
-BIN_FOLDER="$PWD"
-cd "$mydir"
+BIN_FOLDER=`dirname "$0"`
 
 # Assumes java is on the path
 java -cp "$BIN_FOLDER/../lib/*" org.apache.tomcat.jakartaee.MigrationCLI "$@"
