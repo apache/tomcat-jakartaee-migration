1c7748384d92b994099da9003ea19df2df73041d
Romain Manni-Bucau <rmannibucau@gmail.com>
2021-01-02 15:06:28 +0100

    handling method calls too

--- src/main/java/org/apache/tomcat/jakartaee/profile/XmlExclusionAwareProfile.java
+++ src/main/java/org/apache/tomcat/jakartaee/profile/XmlExclusionAwareProfile.java
@@ -4,6 +4,7 @@ import org.apache.bcel.classfile.Utility;
 import org.apache.tomcat.jakartaee.XmlExclusionListBuilder;
 
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class XmlExclusionAwareProfile implements Predicate<String> {
     private final Predicate<String> delegate;
@@ -16,17 +17,20 @@ public class XmlExclusionAwareProfile implements Predicate<String> {
 
     @Override
     public boolean test(final String s) {
-        return !xmlExclusions.getStayInJavax().contains(toClassName(s)) && delegate.test(s);
+        return toClassNames(s).noneMatch(xmlExclusions.getStayInJavax()::contains) && delegate.test(s);
     }
 
-    private String toClassName(String str) {
+    private Stream<String> toClassNames(final String str) {
         if (str.startsWith("<") && str.endsWith(">")) {
-            return str;
+            return Stream.empty();
         }
         try {
-            return Utility.typeSignatureToString(str, false);
+            if (str.startsWith("(")) {
+                return Stream.of(Utility.methodSignatureArgumentTypes(str, false));
+            }
+            return Stream.of(Utility.typeSignatureToString(str, false));
         } catch (final RuntimeException re) {
-            return ""; // will never match
+            return Stream.of();
         }
     }
 }
