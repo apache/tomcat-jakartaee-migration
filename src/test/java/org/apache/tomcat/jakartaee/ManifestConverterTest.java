f1dc093ba38a5d9886f23518a1062ec3cb9aad1f
William McCusker <115317+wmccusker@users.noreply.github.com>
2022-08-22 08:11:15 -0700

    fixed whitespace formating

--- src/test/java/org/apache/tomcat/jakartaee/ManifestConverterTest.java
+++ src/test/java/org/apache/tomcat/jakartaee/ManifestConverterTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class ManifestConverterTest {
 
-	@Test
+    @Test
     public void testAccepts() {
-		ManifestConverter converter = new ManifestConverter();
+        ManifestConverter converter = new ManifestConverter();
 
         assertTrue(converter.accepts("META-INF/MANIFEST.MF"));
         assertTrue(converter.accepts("WEB-INF/bundles/com.example.bundle/META-INF/MANIFEST.MF"));
